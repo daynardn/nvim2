cc6f29afc8f36140ad71ba40de22acfe24fe652b
Nikolas Daynard <Nkdaynard@gmail.com>
2025-01-03 12:18:19 -0500

    Saving finished

--- src/files/files.rs
+++ src/files/files.rs
@@ -20,12 +20,15 @@ pub fn load_file(path: String) -> Vec<SharedString>{
 }
 
 pub fn save(path: String, lines: Vec<SharedString>) {
-    let file =  File::open(path.clone());
+    let file =  File::create(path.clone());
     if file.is_err() {
         println!("{}", path + " NOT FOUND");
     }
     let mut file = file.unwrap();
     for line in lines {
-        file.write(line.as_bytes()).expect("Unable to write data");
+        let mut string: String = line.into();
+        string += "\n";
+        println!("{}", string);
+        file.write(string.as_bytes()).expect("Unable to write data");
     }
 }
